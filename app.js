cb73b16cda8ca6f586830a26bcbad12316193908
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-12-02 10:48:42 +0500

    Initial commit with basic get post

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const fs = require("fs");
+
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
+
+//Get tours Data from the server
+app.get('/api/v1/tours/', (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
+})
+
+//Get tour Data from the server
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1;
+
+    if (id >= tours.length) {
+        return res.status(404).json({
+            status: "failed",
+            message: "Invalid ID"
+        });
+    }
+
+    const tour = tours.find(tr => tr.id === id)
+    res.status(200).json({
+
+        status: "success",
+        results: tours.length,
+        data: {
+            tour
+        }
+    })
+})
+
+// app.get('/', (req, res) => {
+
+//     res.status(200).json({ "app": "test", "message": "URL fetched" });
+
+// });
+
+// Save a tour data on the server
+app.post("/api/v1/tours/", (req, res) => {
+    // console.log(req.body);
+    const newTour = Object.assign({ id: tours[tours.length - 1].id + 1 }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+    });
+
+})
+
+const port = 3000;
+
+
+app.listen(port, () => { console.log(`App running on port ${port}...`) });
