d39e5b5b2929814aa2698158c3db50d0285f2074
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-12-02 12:16:34 +0500

    Router Mounting

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
 
 //CallBack for Tours
 const getAllTours = (req, res) => {
-
     res.status(200).json({
         status: "success",
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours: tours
@@ -27,6 +32,7 @@ const getTour = (req, res) => {
     if (id >= tours.length) {
         return res.status(404).json({
             status: "fail",
+            requestedAt: req.requestTime,
             message: "Invalid ID"
         });
     }
@@ -35,6 +41,7 @@ const getTour = (req, res) => {
     res.status(200).json({
 
         status: "success",
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tour
@@ -51,6 +58,7 @@ const saveTour = (req, res) => {
     fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: "success",
+            requestedAt: req.requestTime,
             data: {
                 tour: newTour
             }
@@ -65,12 +73,14 @@ const updateTour = (req, res) => {
     if (id >= tours.length) {
         return res.status(404).json({
             status: "fail",
+            requestedAt: req.requestTime,
             message: "Invalid ID"
         })
     }
 
     res.status(200).json({
         status: "success",
+        requestedAt: req.requestTime,
         data: {
             tour: "<Updated tour...>"
         }
@@ -84,23 +94,74 @@ const deleteTour = (req, res) => {
     if (id >= tours.length) {
         return res.status(404).json({
             status: "fail",
+            requestedAt: req.requestTime,
             message: "Invalid ID"
         })
     }
 
     res.status(204).json({
         status: "success",
+        requestedAt: req.requestTime,
         data: null
     })
 }
 
+///////////////////////////////////////////////////
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This Route is still not defined on the server"
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This Route is still not defined on the server"
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This Route is still not defined on the server"
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This Route is still not defined on the server"
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This Route is still not defined on the server"
+    })
+}
 
 
+const tourRouter = express.Router();
+const userRouter = express.Router()
+
+
+app.use("/api/v1/tours/", tourRouter);
+app.use("/api/v1/users/", userRouter);
 //Get All tour and Post a Tour
-app.route("/api/v1/tours/").get(getAllTours).post(saveTour);
+tourRouter.route("/").get(getAllTours).post(saveTour);
 
 //Get tour, update it and delete a Tour
-app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+
+//Get All user and create a user
+userRouter.route("/").get(getAllUsers).post(createUser);
+
+//Get user, update it and delete a user
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 const port = 3000;
 
